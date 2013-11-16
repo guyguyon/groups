b40c4eb1c939a5ab10e8d2ed83db1d242e3abb3a
Guy Segev <guysegev@crossrider.com>
2013-11-16 19:56:43 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groups::Application.config.secret_token = 'bedb499e2f401da47ca81a0225243f031dbebd9bf03776e55cccdd1f8f80d0974bfd247c020c7cd4dae00ad479ee6c7bc562403e980a9a2fe09f571a385f4b62'
